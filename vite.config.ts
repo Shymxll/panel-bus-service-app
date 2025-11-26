ce8067d54f206d0b9db1bc6e4a370f5ce4e8ed32
Akram <154376555+akramuhendis@users.noreply.github.com>
2025-11-26 17:00:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,11 +24,10 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3002',
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
